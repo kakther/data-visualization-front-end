681c978d17fdacbe387e294e68974c95dfbf1ba3
Kausar Akther <kausar.akther@gmail.com>
2021-06-07 23:21:05 -0400

    add form

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,5 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react'
 
+const AddForm = () => {
+  return (
+    <div>
+      <h3>Add New Transaction</h3>
+      <form>
+        <div className="form">
+            <label htmlFor="text">Text</label>
+            <input 
+                type="text" 
+                placeholder="Enter text..."
+            />
+        </div>
+            
+        <div className="form">
+            <label htmlFor="amount">Amount <br /> 
+                (negative - expense, positive - income)
+            </label>
+            <input type="number" placeholder="Enter Amount..."/>
+        </div>
+        <button>Add Transaction</button>
 
+      </form>
+    </div>
+  )
+}
 
-
+export default AddForm
