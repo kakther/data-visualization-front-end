f8942e3ec2afdab59de12ebe8624ada6738cb55c
Kausar Akther <kausar.akther@gmail.com>
2021-06-08 21:10:44 -0400

    testing

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
-import React from 'react';
-import Nav from './components/Nav'
-import Header  from './components/Header'
-import Balance from './components/Balance'
-import IncomeExpenses  from './components/IncomeExpenses'
-import TransactionList  from './components/TransactionList'
-import AddForm  from './components/AddForm'
-import Footer  from './components/Footer' 
+import React from 'react'
 
-
-
-import './App.css';
-
-function App() {
-  return (
-    <div>
-      <Nav />    
-      <Header />
-      <div className="container">
-        <Balance />
-        <IncomeExpenses />
-        <TransactionList />
-        <AddForm />
+class App extends React.Component {
+  render = () => {
+    return (
+      <div>
+        <h1>Hello</h1>
       </div>
-      <Footer />
-    </div>
-  );
+    )
+  }
 }
 
-export default App;
+export default App
--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,29 +1,11 @@
 import React from 'react'
+import axios from 'axios'
 
-const AddForm = () => {
-  return (
-    <div>
-      <h3>Add New Transaction</h3>
-      <form>
-        <div className="form">
-            <label htmlFor="text">Text</label>
-            <input 
-                type="text" 
-                placeholder="Enter text..."
-            />
-        </div>
-            
-        <div className="form">
-            <label htmlFor="amount">Amount <br /> 
-                (negative - expense, positive - income)
-            </label>
-            <input type="number" placeholder="Enter Amount..."/>
-        </div>
-        <button>Add Transaction</button>
+export default class AddForm extends Component {
 
-      </form>
-    </div>
-  )
+  render () {
+    return (
+      <div></div>
+    )
+  }
 }
-
-export default AddForm
