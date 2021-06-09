f10fd4db55b7906be559b6fc602f8c58197b5f0d
Kausar Akther <kausar.akther@gmail.com>
2021-06-08 22:57:08 -0400

    update addform

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import React from 'react'
+import Nav from './components/Nav'
+import AddForm from './components/AddForm'
+import Footer from './components/Footer'
 
 class App extends React.Component {
+  state = {
+    name: '',
+    description: '',
+    img: '',
+    cost: '',
+    notes: '',
+    vacation: []
+  }
+
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.id]: event.target.value,
+    });
+  };
+
+  handleChange = (event) => {
+    event.preventDefault();
+    
+  }
+
+
+
+
+
   render = () => {
     return (
       <div>
-        <h1>Hello</h1>
+        <Nav />
+        <AddForm />
+        <Footer />
       </div>
     )
   }
--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react'
 import axios from 'axios'
 
 export default class AddForm extends Component {
 
   render () {
     return (
-      <div></div>
+      <div>
+        <h1>Add your list of Vacation Place</h1> 
+        <form>
+          <div className='container'>
+            <div className='input-box'>
+              <label htmlFor='name'>Name</label>
+              <input type='text' />
+            </div>
+
+            <div className='input-box'>
+              <label htmlFor='name'>Description</label>
+              <input type='text' />
+            </div>
+            <div className='input-box'>
+              <label htmlFor='name'>Image</label>
+              <input type='text' />
+            </div>
+            <div className='input-box'>
+              <label htmlFor='name'>Cost</label>
+              <input type='text' />
+            </div>
+            <div className='input-box'>
+              <label htmlFor='name'>Note</label>
+              <input type='text' />
+            </div>
+            <div className='vacation-button'>
+              <input type='submit' value='Create Vacation List' />
+            </div>
+          </div>
+          
+        </form>
+      </div>
     )
   }
 }
